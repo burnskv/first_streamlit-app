b314a51d6f6106b60a51f0e60cd714689349f5e0
burnskv <104996109+burnskv@users.noreply.github.com>
2023-10-31 16:28:55 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title ('My Parents Healthy Diner')
 
 streamlit.header ('Breakfast Menu')
-streamlit.text('Omega 2 and Porridge')
-streamlit.text('Rocket, Kale and Spinach Tart')
-streamlit.text('Hard-Boiled free-range egg')
+streamlit.text('🥣  Omega 2 and Porridge')
+streamlit.text(' 🥗 Rocket, Kale and Spinach Tart')
+streamlit.text(' 🐔 Hard-Boiled free-range egg')
+streamlit.text('  🥑🍞 Avocado Toast')
