6412234b2794f547b377917fa2672ba6b8164120
burnskv <104996109+burnskv@users.noreply.github.com>
2023-10-31 16:44:20 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ streamlit.title ('My Parents Healthy Diner')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlist.multiselect ("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect ("Pick some fruits:", list(my_fruit_list.index))
 
 streamlit.header ('Breakfast Menu')
 streamlit.text('🥣  Omega 2 and Porridge')
