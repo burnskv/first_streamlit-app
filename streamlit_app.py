a3350694a2eec6d79e96ccdd6287e6218289b75a
burnskv <104996109+burnskv@users.noreply.github.com>
2023-11-05 12:06:04 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,13 @@ import pandas
 
 streamlit.title ('My Parents Healthy Diner')
 
+streamlit.header ('Breakfast Menu')
+streamlit.text('🥣  Omega 2 and Porridge')
+streamlit.text(' 🥗 Rocket, Kale and Spinach Tart')
+streamlit.text(' 🐔 Hard-Boiled free-range egg')
+streamlit.text('  🥑🍞 Avocado Toast')
+
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -11,11 +18,4 @@ my_fruit_list =  my_fruit_list.set_index('Fruit')
 streamlit.multiselect ("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 streamlit.dataframe(my_fruit_list)
-
-streamlit.header ('Breakfast Menu')
-streamlit.text('🥣  Omega 2 and Porridge')
-streamlit.text(' 🥗 Rocket, Kale and Spinach Tart')
-streamlit.text(' 🐔 Hard-Boiled free-range egg')
-streamlit.text('  🥑🍞 Avocado Toast')
-
 
