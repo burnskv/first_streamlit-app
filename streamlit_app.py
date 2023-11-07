4ef6305f10fcc5daf595e6c200ef07424dee7860
burnskv <104996109+burnskv@users.noreply.github.com>
2023-11-07 09:43:17 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
+import requests
 import snowflake.connector
+from urllib.error import URLerror
 
 
 streamlit.title ('My Parents Healthy Diner')
@@ -24,8 +26,6 @@ streamlit.dataframe(fruits_to_show)
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-
-import requests
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
