299b058a7d3e48458480f14782f3dcb3283284f8
burnskv <104996109+burnskv@users.noreply.github.com>
2023-11-07 11:56:21 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,20 +47,20 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-def get_fruityjuice_data(this_fruit_choice):
-      fruityjuice_response = requests.get("https://fruityjuice.com/api/fruit/" + this_fruit_choice)
-      fruityjuice_normalized = pandas.json_normalize(fruityjuice_response.json())
-      return fruityjuice_normalized
+def get_fruityvice_data(this_fruit_choice):
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      return fruityvice_normalized
 
 
 streamlit.header ('Fruityvice Fruit advice!')
 
 try:
-  fruit_choice = streamlit.text_input('What furit would you like information about?')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
        streamlit.error("please select a fruit to get information.") 
   else:
-       back_from_function = get_fruityjuice_data(fruit_choice)
+       back_from_function = get_fruityvice_data(fruit_choice)
        # fruityvice_respons = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
        # fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
        streamlit.dataframe(back_from_function)
